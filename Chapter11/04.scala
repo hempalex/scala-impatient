a423503250ece296c696f7d723e9921380fda13f
Alexander Mikhailov <support@indico.ru>
2012-08-15 02:12:05 +0400

    More elegant solution for 11.4

--- Chapter11/04.scala
+++ Chapter11/04.scala
@@ -1,14 +1,15 @@
-class Money(d: Int, c: Int) {
-	val dollars: Int = d + c / 100
-	val cents: Int = c % 100
+class Money(d: BigInt, c: BigInt) extends Ordered[Money]{
+	val dollars: BigInt = d + c / 100
+	val cents: BigInt = c % 100
 	override def toString() = "%d.%d".format(dollars, cents)
 
-	def ==(that: Money) = this.dollars == that.dollars && this.cents == that.cents
-	def +(that: Money) = new Money(this.dollars + that.dollars, this.cents + that.cents)
-	def -(that: Money) = {
-		val cents = this.dollars * 100 + this.cents - that.dollars * 100 - that.cents
-		new Money(cents / 100, cents % 100)
-	}
+	def toCents(): BigInt = dollars * 100 + cents
+	def fromCents(cents: BigInt) = new Money(cents / 100, cents % 100)
+
+	override def compare(that: Money):Int = toCents.compare(that.toCents)
+
+	def +(that: Money) = fromCents(this.toCents + that.toCents)
+	def -(that: Money) = fromCents(this.toCents - that.toCents)
 }
 
 object Money {
@@ -20,4 +21,6 @@ val y = Money(0, 50)
 
 println(x + y)
 println(x - y)
+
+println(x > y)
 
