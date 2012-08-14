12c8db055236be454f516469b688675f64eff7a6
Alexander Mikhailov <support@indico.ru>
2012-08-14 18:30:30 +0400

    Right solutions for Chapter 10, exercises 8 & 9

--- Chapter10/09.scala
+++ Chapter10/09.scala
@@ -1,9 +1,20 @@
-// read http://stackoverflow.com/questions/10169016/stackoverflow-when-use-bufferedinputstream-as-a-trait-in-scala
 import java.io.{InputStream, FileInputStream}
+
+trait Logger {
+	def log(msg: String)
+}
+
+trait NoneLogger extends Logger {
+	def log(msg: String) = {}
+}
+
+trait PrintLogger extends Logger {
+	def log(msg: String) = println(msg)
+}
 
 
 trait Buffering {
-	this: InputStream =>
+	this: InputStream with Logger =>
 
 	val BUF_SIZE: Int = 5
 	private val buf = new Array[Byte](BUF_SIZE)
@@ -13,6 +24,7 @@ trait Buffering {
 	override def read(): Int = {
 		if (pos >= bufsize) {
 			bufsize = this.read(buf, 0, BUF_SIZE)
+			log("buffered %d bytes: %s".format(bufsize, buf.mkString(", ")))
 			if (bufsize > 0) -1
 			pos = 0
 		}
@@ -21,6 +33,6 @@ trait Buffering {
 	}
 }
 
-val f = new FileInputStream("08.txt") with Buffering
+val f = new FileInputStream("08.txt") with Buffering with PrintLogger
 
 for(i <- 1 to 10) println(f.read())
