cc0cb8b4f5cf5ad297e6d7558a20fc440f116f1c
Alexander Mikhailov <support@indico.ru>
2012-08-17 01:19:23 +0400

    11.8

--- Chapter11/08.scala
+++ Chapter11/08.scala
@@ -1,19 +1,60 @@
 class Matrix(val m: Int, val n: Int) {
+	
 	private val value = Array.ofDim[Double](m, n)
 
 	def update(x: Int, y: Int, v: Double) = value(x)(y) = v
 	def apply(x: Int, y: Int) = value(x)(y)
 
-	def *(factor: Double) {
-		for(row <- value) yeild (for (col <- row) yield col*factor)
-	}
-
-	def *(other: Matrix) {
-		require(n == other.m)
-	}
-
-	def +(other: Matrix) {
+	def +(other: Matrix) = {
 		require (n == other.n)
 		require (m == other.m)
+
+		var res = new Matrix(m, n)
+		for(i <- 0 until m; j <- 0 until n) {
+			res(i, j) = this.value(i)(j) + other.value(i)(j)
+		}
+		res 
 	}
+
+	def -(other: Matrix) = this + other * -1
+
+
+
+	def *(factor: Double) = {
+		var res = new Matrix(m, n)
+		for(i <- 0 until m; j <- 0 until n) {
+			res(i, j) = this.value(i)(j) * factor
+		}
+		res 
+	}
+
+	private def prod(other: Matrix, i: Int, j: Int) = {
+		(for (k <- 0 until n) yield value(i)(k) * other.value(j)(k)).sum
+	}
+
+	def *(other: Matrix) = {
+		require(n == other.m)
+		var res = new Matrix(m, n)
+		for(i <- 0 until m; j <- 0 until n) {
+			res(i, j) = prod(other, i, j)
+		}
+		res 
+	}
+	
+	override def toString = value.map(_.mkString(" ")).mkString("\n")
 }
+
+val x = new Matrix(2, 2)
+x(0, 0) = 1
+x(0, 1) = 2
+x(1, 0) = 3
+x(1, 1) = 4
+
+println(x)
+println()
+println(x * 2)
+println()
+println(x * 2 - x)
+println()
+
+println((x * 2) * (x * 3))
