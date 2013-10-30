a822146f9e8b3b6a9ed96235dc8a1126296dbec6
Alexander Mikhailov <support@indico.ru>
2013-10-30 22:33:05 +0400

    More elegant solution for 13.7

--- Chapter13/07.scala
+++ Chapter13/07.scala
@@ -1,7 +1,6 @@
-val prices = List(5.0, 20.0, 9.95) 
+val prices = List(5.0, 20.0, 9.95)
 val quantities = List(10, 2, 1)
 
-val subtotal = ((x: Double, y: Int) => x * y).tupled
-val sum = (prices zip quantities) map(subtotal(_))
+val sum = (prices, quantities).zipped map { _ * _ }
 
 println(sum)
