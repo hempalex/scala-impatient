1eccc758727828252de8d6e8160cc19589ed1efc
fouadnashat <fouad.ali@live.com>
2014-05-26 16:56:52 +0200

    Update 01.scala

--- Chapter03/01.scala
+++ Chapter03/01.scala
@@ -2,7 +2,7 @@ import util.Random
 
 def randomArray(n: Int) = {
 	val a = new Array[Int](n);
-	for (i <- 0 until a.size) a(i) = Random.nextInt(n);
+	for (i <- 0 until a.length) a(i) = Random.nextInt(n);
 	a
 }
 
