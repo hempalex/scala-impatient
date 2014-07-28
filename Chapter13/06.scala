123fe3f6b7d66b72ec3f325932ae3c9dcb56ad73
Kristof A <kristof.adriaenssens@gmail.com>
2014-07-28 13:08:43 +0200

    Fix comments.
    Correct foldLeft and foldRight comments.

--- Chapter13/06.scala
+++ Chapter13/06.scala
@@ -1,8 +1,8 @@
 val lst = List(1, 2, 3, 4, 5)
 println(lst)
-val x = (lst :\ List[Int]())(_ :: _) // foldLeft
+val x = (lst :\ List[Int]())(_ :: _) // foldRight
 println(x)
-val y = (List[Int]() /: lst)(_ :+ _) // foldRight
+val y = (List[Int]() /: lst)(_ :+ _) // foldLeft
 println(y)
 val z = (List[Int]() /: lst)((x,y) => y :: x)
 println(z)
