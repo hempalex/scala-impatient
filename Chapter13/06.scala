0928436c178f71b09fb5bfcab4cc35135dc529f6
Kristof A <kristof.adriaenssens@gmail.com>
2014-07-28 13:10:00 +0200

    Add reversing of list using foldRight

--- Chapter13/06.scala
+++ Chapter13/06.scala
@@ -4,5 +4,7 @@ val x = (lst :\ List[Int]())(_ :: _) // foldRight
 println(x)
 val y = (List[Int]() /: lst)(_ :+ _) // foldLeft
 println(y)
-val z = (List[Int]() /: lst)((x,y) => y :: x)
+val z = (List[Int]() /: lst)((x,y) => y :: x) // reverse list using foldLeft
 println(z)
+val z2 = (lst :\ List[Int]())((x,y) => y :+ x) // reverse list using foldRight
+println(z2)
