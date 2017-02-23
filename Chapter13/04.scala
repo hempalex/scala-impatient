df5da3a457c73d1dbc11f356c62545585eac97ee
Lars Kollmann <larsko1994@gmail.com>
2017-02-23 15:44:32 +0100

    Removed unnecessary call to map

--- Chapter13/04.scala
+++ Chapter13/04.scala
@@ -1,8 +1,8 @@
 
-def existIn(a: Array[String], m: Map[String, Int]) = a.toList.map(m.get(_)).flatMap(x => x)
+def existIn(a: Array[String], m: Map[String, Int]) = a.flatMap(m.get).toList
 
-val a = Array("Tom", "Fred", "Harry") 
-val m = Map("Tom" -> 3, "Dick" -> 4, "Harry" -> 5) 
+val a = Array("Tom", "Fred", "Harry")
+val m = Map("Tom" -> 3, "Dick" -> 4, "Harry" -> 5)
 
 val x = existIn(a, m)
 println(x)
